ca7fdede4b083dd36b88c8cd34886b7e22e42b3d
Gokul <glp.gokulpatil@gmail.com>
2024-09-30 13:58:13 +0530

    updated with get and post request

--- serializerpro/app1/api/serializers.py
+++ serializerpro/app1/api/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
-
+from app1.models import Coder
 class CoderSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     first_name = serializers.CharField(max_length=55)
     exp = serializers.IntegerField()
     skills = serializers.CharField(max_length=66)
  
+    def create(self, validated_data):
+
+        return Coder.objects.create(**validated_data)
--- serializerpro/app1/api/views.py
+++ serializerpro/app1/api/views.py
@@ -4,14 +4,26 @@ from app1.models import Coder
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 # Create your views here.
-@api_view()
+@api_view(['GET', 'POST'])
 def coderlist(request):
-    c=Coder.objects.all()
-    serializer=CoderSerializer(c,many=True)
-    return Response(serializer.data)
+    if request.method=='GET':
+
+        c=Coder.objects.all()
+        serializer=CoderSerializer(c,many=True)
+        return Response(serializer.data)
+    if request.method=='POST':
+        serializer=CoderSerializer(data=request.data)
+        print(serializer)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
 
 @api_view()
 def coder_data(request,pk):
     c=Coder.objects.get(pk=pk)
     serializer=CoderSerializer(c)
     return Response(serializer.data)
+
