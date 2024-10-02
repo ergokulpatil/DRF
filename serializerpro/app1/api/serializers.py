13823c30cb8939ecdcd3bcdee49878ec918fc572
Gokul <glp.gokulpatil@gmail.com>
2024-10-02 10:47:57 +0530

    Status code updated

--- serializerpro/app1/api/serializers.py
+++ serializerpro/app1/api/serializers.py
@@ -9,3 +9,10 @@ class CoderSerializer(serializers.Serializer):
     def create(self, validated_data):
 
         return Coder.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.exp = validated_data.get('exp', instance.exp)
+        instance.skills = validated_data.get('skills', instance.skills)
+        instance.save()
+        return instance
--- serializerpro/app1/api/views.py
+++ serializerpro/app1/api/views.py
@@ -3,11 +3,12 @@ from app1.api.serializers import CoderSerializer
 from app1.models import Coder
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 # Create your views here.
 @api_view(['GET', 'POST'])
 def coderlist(request):
     if request.method=='GET':
-
+        
         c=Coder.objects.all()
         serializer=CoderSerializer(c,many=True)
         return Response(serializer.data)
@@ -18,12 +19,29 @@ def coderlist(request):
             serializer.save()
             return Response(serializer.data)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view()
+@api_view(['GET', 'PUT','DELETE'])
 def coder_data(request,pk):
-    c=Coder.objects.get(pk=pk)
-    serializer=CoderSerializer(c)
-    return Response(serializer.data)
+    if request.method=='GET':
+        try:
+            c=Coder.objects.get(pk=pk)
+        except Coder.DoesNotExist:
+            return Response({'error':'Coder record not found'},status=status.HTTP_404_NOT_FOUND) 
+               
+        serializer=CoderSerializer(c)
+        return Response(serializer.data)
+    if request.method=='PUT':
+        c=Coder.objects.get(pk=pk)
+        serializer=CoderSerializer(c,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    if request.method=="DELETE":
 
+        c=Coder.objects.get(pk=pk)
+        c.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
